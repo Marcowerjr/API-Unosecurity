2022d66b45613b35e0a2acc8b5543e418b56c1fb
Marcos Caballero <marcowerjr@gmail.com>
2021-03-24 19:17:31 -0700

    Update database

--- db_config.py
+++ db_config.py
@@ -7,8 +7,5 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
 
-client = pymongo.MongoClient("mongodb+srv://<username>:<password>@cluster0.en1r6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://{DB_USER}>:{DB_PASSWORD}@bd.3v6dc.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
 db = client.users
-
-
-
