e50819f8bd85cb33bed2ed8090e90806a7a2ce69
Marcos Caballero <marcowerjr@gmail.com>
2021-04-06 22:39:02 -0600

    Update routes

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from flask_pymongo import pymongo
 from app import create_app
 from bson.json_util import dumps
 import db_config as db
-
+import os 
+TOKEN = os.getenv("TOKEN")
 app = create_app()
 
 @app.route ('/test/')
@@ -12,15 +13,14 @@ def test():
 "message":  "Cette API travail bien."
     })
 
-@app.route('/admin/users/token', methods=['GET'])
 #implementar que solo el master pueda acceder a esta ruta
+@app.route('/admin/users/{TOKEN}/all', methods=['GET'])
 def show_users():
     all_users = list(db.db.users.find())
     for users in all_users:
        del users ["_id"]
     return jsonify({"all_users":all_users})
     
-
 
 @app.route('/users/<int:n_top>/', methods=['GET'])
 def show_a_top_users(n_top):
@@ -49,6 +49,7 @@ def add_new_users():
 
 @app.route('/api/top_users/update/<int:n_top>',methods=['PUT'])
 def update_users(n_top):
+
     if db.db.users.find_one({'n_top':n_top}):
         db.db.users.update_one({'n_top':n_top},
         {'$set':{   
@@ -60,6 +61,7 @@ def update_users(n_top):
         }})
     else:
         return jsonify({"status":400, "message": f"the user #{n_top} not found"})
+
     return jsonify({"status":200, "message": f"The user #{n_top} has been updated successfully"})
 
 
@@ -70,6 +72,51 @@ def delete_users(n_top):
     else:
         return jsonify({"status":400, "message": f"the user #{n_top} not found"})
     return jsonify({"status":200, "message": f"The user #{n_top} has been deleted successfully"})
+
+
+
+#Ruta para crear nuevos dispositivos.
+@app.route('/api/new_device/', methods=['POST'])
+def add_new_device():
+    db.db.device.insert_one({
+        "n_top": request.json["n_top"],
+        "status":request.json["status"],
+        "latitude":request.json["latitude"],
+        "longitude":request.json["longitude"],
+        "tilt":request.json["tilt"]       
+    })
+    return jsonify({
+        "message":"Se añadio correctamente un nuevo dispositivo",
+        "status": 200,
+    })
+
+#Actualizar nuestro dispositivo
+@app.route('/api/top_device/update/<int:n_top>',methods=['PUT'])
+def update_device(n_top):
+    if db.db.device.find_one({'n_top':n_top}):
+        db.db.device.update_one({'n_top':n_top},
+        {'$set':{           
+        "n_top": request.json["n_top"],
+        "status":request.json["status"],
+        "latitude":request.json["latitude"],
+        "longitude":request.json["longitude"],
+        "tilt":request.json["tilt"]
+        }})
+    else:
+        return jsonify({"status":400, "message": f"the device #{n_top} not found"})
+
+    return jsonify({"status":200, "message": f"The device #{n_top} has been updated successfully"})
+
+
+#Obtener
+@app.route('/device/<int:n_top>/', methods=['GET'])
+def show_a_top_device(n_top):
+    device = db.db.device.find_one({'n_top':n_top})
+    del device ["_id"]
+
+    return jsonify({
+            "device":device
+        })
 
 if __name__ == '__main__':
     app.run(load_dotenv=True, port=8080) 
